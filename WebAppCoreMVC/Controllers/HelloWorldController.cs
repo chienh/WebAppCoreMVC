6d3f46a934937ab6ac3d293622568d5a72124ebf
Chien Hsiao <chienh@gmail.com>
2022-11-25 15:52:46 -0300

    Add project files.

---
+++ WebAppCoreMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebAppCoreMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes)
+        {
+            ViewData["msg"] = "Olá, " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
